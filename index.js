7f788e294414775bfaaa659c4fb198b33c559a55
tepolito <tepolito55@gmail.com>
2017-10-22 19:18:03 -0400

    functional app

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 const YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3/search"
 
+function getAPIData(searchTerm, callBack)
+{
+	const query = {
+		q: `${searchTerm} in:name`,
+		part:`snippet`,
+		key:'AIzaSyAIpBNlyyGDv4dPtB_eKDg-JIbpnhKlDCY',
+	}
+
+	$.getJSON(YOUTUBE_API_URL, query, callBack);
+
+}
+
+
+function displayAPIData(data)
+{
+	const results = data.items.map(function (item, index)
+	{
+		return `<div>
+	<img src="${item.snippet.thumbnails.default.url}">
+	</div>`;
+	})
+
+	console.log(results[1]);
+
+	$('.js-results').html(results);
+}
+
 function watchSubmit()
 {
 	$('.js-search-form').submit(function (event)
@@ -13,6 +40,8 @@ function watchSubmit()
 		queryText.val('');
 
 		console.log(query);
+
+		getAPIData(query, displayAPIData);
 	})
 }
 
